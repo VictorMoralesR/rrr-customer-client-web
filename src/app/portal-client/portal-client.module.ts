70d622a2434a90676aa55b6072f543858d0200d2
Victor Morales <vmorales@anzen.com.mx>
2020-06-17 17:03:28 -0500

    feat: new module added

---
+++ src/app/portal-client/portal-client.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PortalClientRoutingModule } from './portal-client-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PortalClientRoutingModule
+  ]
+})
+export class PortalClientModule { }
