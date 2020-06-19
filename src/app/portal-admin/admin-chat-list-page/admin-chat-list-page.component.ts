d1b80e227025d6bfa7ce885a2d3719a833c6a127
Victor Morales <vmorales@anzen.com.mx>
2020-06-19 18:06:04 -0500

    feat: chat list to conversation

--- src/app/portal-admin/admin-chat-list-page/admin-chat-list-page.component.ts
+++ src/app/portal-admin/admin-chat-list-page/admin-chat-list-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-chat-list-page',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminChatListPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+  goToConversation(){
+    this.router.navigate(['portal/admin/conversation']);
   }
 
 }
