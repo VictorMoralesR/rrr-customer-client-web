e43678d8b9e8b5f889cd97ae77fea526725ca4eb
Victor Morales <vmorales@anzen.com.mx>
2020-06-19 12:28:05 -0500

    feat: login header and admin home page added

--- src/app/portal-admin/portal-admin-routing.module.ts
+++ src/app/portal-admin/portal-admin-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLoginPageComponent } from './admin-login-page/admin-login-page.component';
+import { AdminHomePageComponent } from './admin-home-page/admin-home-page.component';
 
 const routes: Routes = [
   { 
     path: 'portal/admin',
     component: AdminLoginPageComponent,
+  },
+  { 
+    path: 'portal/admin/home',
+    component: AdminHomePageComponent,
   },
 ];
 
--- src/app/portal-admin/portal-admin.module.ts
+++ src/app/portal-admin/portal-admin.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//modules 
 import { PortalAdminRoutingModule } from './portal-admin-routing.module';
+import { SupportModule } from './../support.module';
+//components
 import { AdminLoginPageComponent } from './admin-login-page/admin-login-page.component';
+import { AdminHomePageComponent } from './admin-home-page/admin-home-page.component';
+import { AdminHeaderComponent } from './admin-header/admin-header.component';
 
 
 @NgModule({
-  declarations: [AdminLoginPageComponent],
+  declarations: [AdminLoginPageComponent, AdminHomePageComponent, AdminHeaderComponent],
   imports: [
     CommonModule,
-    PortalAdminRoutingModule
+    PortalAdminRoutingModule,
+    SupportModule
   ]
 })
 export class PortalAdminModule { }
